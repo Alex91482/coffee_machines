075e14385524a1dec32e96b6e67df417626acb0f
Alexei Kubarev <kopernik.forwator@gmail.com>
2023-10-15 09:40:22 +0300

    fix constructor

--- coffee_machines_WebFlux/src/main/java/com/example/demo/service/beverages/DoubleEspresso.java
+++ coffee_machines_WebFlux/src/main/java/com/example/demo/service/beverages/DoubleEspresso.java
@@ -3,11 +3,6 @@ package com.example.demo.service.beverages;
 
 public class DoubleEspresso extends AbstractCoffeeBeverages implements Beverages {
 
-    public DoubleEspresso(){
-        final int waterConsumption = getWaterConsumption(); // 100 мл воды
-        final int coffeeConsumption = getCoffeeConsumption(); // 20 мл кофе
-    }
-
     private final int waterConsumption = 100; // 100 мл воды
     private final int coffeeConsumption = 20; // 20 мл кофе
 
--- coffee_machines_WebFlux/src/main/java/com/example/demo/service/beverages/Espresso.java
+++ coffee_machines_WebFlux/src/main/java/com/example/demo/service/beverages/Espresso.java
@@ -2,13 +2,6 @@ package com.example.demo.service.beverages;
 
 public class Espresso extends AbstractCoffeeBeverages implements Beverages {
 
-    public Espresso(){
-        final int waterConsumption = getWaterConsumption();
-        final int coffeeConsumption = getCoffeeConsumption();
-    }
-
-
-
     private final int waterConsumption = 100; // 100 мл воды
     private final int coffeeConsumption = 10; // 10 гр кофе
 
